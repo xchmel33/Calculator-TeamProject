cfd28b54b243a978f5849ee8b33ceec7b4273933
xchmel33 <62353125+xchmel33@users.noreply.github.com>
2020-04-29 12:24:43 +0200

    documented calc

--- src/calc.py
+++ src/calc.py
@@ -1,21 +1,32 @@
-######################
-# IVS - team project #
-#     Calculator     #
-######################
-#       Authors:     #
-# xchmel33, xhurba03 #
-######################
-
-#import main.py
-#import calc.py
-
-import re
+"""This module contains all functions required for the calculator
+"""
 import math
 
-#calc.py:
+def isfloat(element):
+    """Function determines if the input string contains a float number
 
-#function operates all additons in expression(string)
+    :param element: input from other functions
+    :type element: string
+    :return: true if input contains a float number, false if it doesn't
+    """
+
+    element = str(element).replace('.', '', 1)
+    element = str(element).replace('-', '', 1)
+
+    if element.isdigit():
+        return True
+    else:
+        return False
+
 def addition(expression):
+    """Function calculates all additions in the expression,
+    replaces operation with result and deletes operands
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :return: updated list where all additions were calculated
+    """
 
     if expression[0]=='+':
         del expression[0]
@@ -23,237 +34,229 @@ def addition(expression):
     i = 1
     while i!=len(expression):
 
-        #find operation
         if expression[i] == "+":
 
-            #convert string to float
-            x=float(expression[i-1])
-            y=float(expression[i+1])
-
-            #additon
-            expression[i]=round(x+y,10)
-
-            #remove solved parts of string in expression
+            expression[i]=round(float(expression[i-1])+float(expression[i+1]),10)
             expression.remove(expression[i-1])
             expression.remove(expression[i])
 
         else:
             i+=1
 
-    #return updated expression(string) with results from addition
     return expression
 
-#function finds negative numbers and replaces subtraction
-#with addition of opposite number in expression(string)
 def subtraction(expression):
+    """Function firstly finds negative numbers in expression,
+    deletes operation, makes the number negative and in case
+    there is a number before the operation inserts '+'
+    before negative number so it can be later calculated in the addition function
+
+    :param expression: input, negative numbers are found first
+    :type expression: list
+    :param i: index
+    :return: updated list where all negative numbers are found and subtractions replaced with additions
+    """
 
     i=1
     while i!=len(expression):
 
-        #find negative number
-        if str(expression[i]).replace('.', '', 1).isdigit() and expression[i-1]=="-":
+        if isfloat(expression[i]) and expression[i-1]=="-":
             expression[i]=-float(expression[i])
             del expression[i-1]
 
-            #in case of subtraction
-            if i>1 and str(expression[i-2]).replace('.', '', 1).isdigit():
+            if i>1 and isfloat(expression[i-2]):
                 expression.insert(i-1, "+")
 
         else:
             i+=1
 
-    #return updated array expression with negative numbers
     return expression
 
-#function operates all multiplications in array expression
 def multiplication(expression):
+    """Function calculates all multiplications in the expression,
+    replaces operation with result and deletes operands
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :return: updated list where all multiplications were calculated
+    """
 
     i = 1
     while i!=len(expression):
 
-        #find operation
         if expression[i] == "*":
 
-            #convert string to float
-            x=float(expression[i-1])
-            y=float(expression[i+1])
-
-            #multiplication
-            expression[i]=round(x*y,10)
-
-            #remove solved parts of string in expression
+            expression[i]=round(float(expression[i-1])*float(expression[i+1]),10)
             del expression[i-1]
             del expression[i]
 
         else:
             i+=1
 
-    #return updated array expression with results from multiplication
     return expression
 
-#function operates all divisions in array expression
 def division(expression):
+    """Function calculates all divisions in the expression,
+    replaces operation with result and deletes operands
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :raises ValueError: cannot divide by zero
+    :return: updated list where all divisions were calculated
+    """
 
     i = 1
     while i!=len(expression):
 
-        #find operation
         if expression[i] == "/":
 
-            #convert string to float
-            x=float(expression[i-1])
-            y=float(expression[i+1])
-
-            #error check
-            if y == 0:
+            if float(expression[i+1]) == 0:
                 raise ValueError("Division by zero")
 
-            #division
-            expression[i]=round(x/y,10)
-
-
-            #remove solved parts of string in expression
-            expression.remove(expression[i-1])
-            expression.remove(expression[i])
-
-        else:
-            i+=1
-
-    #return updated array expression with results from division
-    return expression
-
-#function operates all factiorials in array expression
-def factorial(expression):
-
-    i = 1
-    while i!=len(expression):
-
-        #find operation
-        if expression[i] == "!":
-
-            #convert string to float
-            x=int(expression[i-1])
-
-            #error check
-            if x<0:
-                raise ValueError("Factorial must be only for numbers greater or equal to 0")
-
-            #factiorial
-            fact = 1
-            for k in range(1,x+1):
-                fact = fact * k
-            expression[i-1]=fact
-
-            #remove solved parts of string in array expression
+            expression[i]=round(float(expression[i-1])/float(expression[i+1]),10)
+            del expression[i-1]
             del expression[i]
 
         else:
             i+=1
 
-    #return updated array expression with results from factorial
     return expression
 
-#function operates all powers in array expression
+def factorial(expression):
+    """Function calculates all factorials in the expression,
+    replaces operand with result and deletes operation
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :param fact: gradually updated calculation of factorial
+    :type fact: int
+    :raises ValueError: factorial can be calculated only for natural numbers
+    :return: updated list where all factorials were calculated
+    """
+
+    i = 1
+    while i!=len(expression):
+
+        if expression[i] == "!":
+
+            if not str(expression[i-1]).isdigit():
+                raise ValueError("Factorial must be only for integers greater or equal to 0")
+
+            fact = 1
+            for k in range(1,int(expression[i-1])+1):
+                fact = fact * k
+            expression[i-1]=fact
+            del expression[i]
+
+        else:
+            i+=1
+
+    return expression
+
 def power(expression):
+    """Function calculates all powers in the expression,
+    replaces operation with result and deletes operands
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :raises ValueError: exponent cannot be negative
+    :return: updated list where all powers were calculated
+    """
 
     i = 1
     while i!=len(expression):
         if expression[i] == "^":
 
-            #convert string to float
-            x=float(expression[i-1])
-            n=float(expression[i+1])
+            if float(expression[i+1])<0:
+                raise ValueError("Exponent has to be greater or equal to 0")
 
-            #error check
-            #if x<0:
-            #    raise ValueError("Exponent has to be integer, which is greater or equal 0")
-
-            #power calculation
-            expression[i]=round(x**n,10)
-
-            #remove solved parts of string in array expression
+            expression[i]=round(float(expression[i-1])**float(expression[i+1]),10)
             del expression[i-1]
             del expression[i]
 
         else:
             i+=1
 
-    #return updated array expression with results from power
     return expression
 
-#function operates all roots in array expression
 def root(expression):
+    """Function calculates all roots in the expression,
+    (root without argument before operation is considered as square root,
+    root from negative numbers is separated)
+    replaces operation with result and deletes operands
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :param x: radicand
+    :type x: float
+    :param n: degree
+    :type n: float
+    :raises ValueError: root degree cannot be negative, pair root degrees must have positive radicands
+    :return: updated list where all roots were calculated
+    """
 
     i = 0
     while i!=len(expression):
         if expression[i] == "r":
 
-            #root with both arguments
-            if i>0 and str(expression[i-1]).replace('.', '', 1).isdigit():
+            if i>0 and isfloat(expression[i-1]):
                 n=float(expression[i-1])
                 x=float(expression[i+1])
 
-            #defalut root:
             else:
                 n=2
                 x=float(expression[i+1])
-                
-                #array maintenance
                 expression.insert(i+1,0)
                 i+=1
 
-            #negative root
             negative = False
             if x<0 and n%2==1:
                 negative = True
                 x=-x
 
-
-            #error check
             if x<0 and n%2==0:
-                raise ValueError("Sqaure roots can be only from possitive numbers")
+                raise ValueError("Roots with pair degrees can be only calculated from positive radicands")
             if n<0:
-                raise ValueError("Root has to be integer or equal to 0")
+                raise ValueError("Root degree has to be integer greater or equal to 0")
 
-            #root calculation
             expression[i]=x**(1/n)
+
             if negative:
                 expression[i]=-expression[i]
 
-            #remove solved parts of string in array expression
             del expression[i-1]
             del expression[i]
 
         else:
             i+=1
 
-    #return updated array expression with results from root
     return expression
 
-#+lub funkce
-#function operates all logartims in array expression
 def logarithm(expression):
+    """Function calculates all common logarithms in the expression
+    replaces operation with result, deletes operand and other unwanted strings
+
+    :param expression: input, so far calculated expression from other functions
+    :type expression: list
+    :param i: index
+    :raises ValueError: cannot make logarithm with negative antilogarithm argument
+    :return: updated list where all logarithms were calculated
+    """
 
     i = 0
     while i!=len(expression):
 
-        #find operation
         if expression[i] == "l" and expression[i+1] == "o" and expression[i+2]=="g":
 
-            if str(expression[i+3]).replace('.', '', 1).isdigit():
-                a=10
-                x=float(expression[i+3])
+            if float(expression[i+3]) <= 0:
+                raise ValueError("Logarithm antilogarithm argument has to be greater than 0")
 
-            #error check
-            if x <= 0:
-                raise ValueError("Logarithm x argument has to be greater than 0")
-
-            #log calculation
-            expression[i]=round(math.log(x,a))
-
-            #remove solved parts of string in array expression
-            if i+4 != len(expression) and str(expression[i+4]).replace('.', '', 1).isdigit():
-                del expression[i+4]
+            expression[i]=math.log(float(expression[i+3]),10)
             del expression[i+3]
             del expression[i+2]
             del expression[i+1]
@@ -261,36 +264,46 @@ def logarithm(expression):
         else:
             i+=1
 
-    #return updated array expression with results from logaritm
     return expression
 
-#function calculates the number of opened parenthesis in given array
 def max_parenthesis(expr):
+    """Function searches the expression for parenthesis
+    
+    :param expr: input expression
+    :type expr: list
+    :param opened: opened parenthesis counter
+    :type opened: int
+    :param max: maximum opened parenthesis counter
+    :return: number of most opened parenthesis
+    """
 
-   opened = 0
-   max = 0
-   for i in range(len(expr)):
-       if expr[i]=="(":
+    opened = 0
+    max = 0
+    for i in range(len(expr)):
+        if expr[i]=="(":
            opened+=1
-       if expr[i]==")":
+        if expr[i]==")":
            opened-=1
-       if opened > max:
+        if opened > max:
            max=opened
 
-   #return the number of most opened parenhesis
-   return max
+        if opened<0:
+           raise ValueError("Wrong parenthesis")
+    return max
 
-#function calculates the inside of deepest parenthesis
 def calc_parenthesis(expr,tph):
+    """Function calculates the inside of deepest parenthesis
+
+    :param expr: input expression
+    :type expr: list
+    :param tph: maximum number of parenthesis in expression
+    :type tph: int
+    :return: updated expression with result from the deepest parenthesis
+    """
 
     opened = closed = i  = 0
     new_elements = []
 
-    #errors check
-    if expr[0]==")":
-        raise ValueError("Wrong start parenthesis )")
-
-    #
     while i != len(expr):
         if expr[i]=="(":
             opened+=1
@@ -301,79 +314,53 @@ def calc_parenthesis(expr,tph):
         diff=opened-closed
         diff/=2
 
-        #inside deepest parenthesis
         if diff == tph:
 
-            #single num inside
             if expr[i]=="(" and expr[i+2]==")":
                 del expr[i+2]
                 del expr[i]
                 break
 
-            #delete start parenthesis
             if expr[i]=="(":
                 del expr[i]
 
-            #at the end
             elif expr[i+1]==")":
 
-                #delete parenthesis
                 del expr[i+1]
-
-                #save to new array and delete old
                 new_elements.append(expr[i])
                 del expr[i]
-
-                #get result of inside parethesis
                 x=calc_inside(new_elements)
                 expr.insert(i,x)
                 break;
 
-            #save to new array and delete old
             new_elements.append(expr[i])
             del expr[i]
 
         else:
             i+=1
 
-    #return updated array without deepest parenthesis
     return expr
 
 def calc_inside(expression):
+    """Function by calling other functions in mathematical order
+    updates expression until the result is one number
 
-    #factorial
-    result = factorial(expression)
-    print("result after factorial="+str(result))
+    :param expression: input expression to be updated
+    :type expression: list
+    :return: result from all functions
+    """
+    
+    result = expression
+    while len(expression)!=1:
 
-    #change subtraction to negative
-    result = subtraction(result)
-    print("result after negative="+str(result))
+        result = factorial(result)
+        result = subtraction(result)
+        result = logarithm(result)
+        result = root(result)
+        result = power(result)
+        result = multiplication(result)
+        result = division(result)
+        result = addition(result)
 
-    #logaritm
-    result = logarithm(result)
-    print("result after log="+str(result))
-
-    #root
-    result = root(result)
-    print("result after root="+str(result))
-
-    #power
-    result = power(result)
-    print("result after power="+str(result))
-
-    #multiply
-    result = multiplication(result)
-    print("result after mul="+str(result))
-
-    #division
-    result = division(result)
-    print("result after div="+str(result))
-
-    #addition
-    result=addition(result)
-    print("result after add="+str(result))
-
-    return result[0]
-
-def sqrt(number):
-    return math.sqrt(number)
+    else:
+        return result[0]
